d14c32762c23cb62422c11aa01930e44ca7d987a
wangkuixing <wangkx1990@sina.com>
2019-04-01 20:03:53 +0800

    add allure2

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,31 +1,66 @@
 
+import io.qameta.allure.*;
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.StepResult;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.AssertJUnit;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 public class NewTest {
 
-    private WebDriver driver;
+    static WebDriver driver;
+
+    @Test(description = "test baidu name")
+    @Description("Some detailed test description")
+    @Link("https://baidu.com")
+    @Severity(SeverityLevel.CRITICAL)
+    void demo1(){
+        login();
+        login();
+        try {
+            Allure.addAttachment("demo picture", "image/jpeg", new FileInputStream("/Users/kevinwang/Downloads/example1.jpeg"), ".jpeg");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        driver.get("http://baidu.com");
+        String tittle=driver.getTitle();
+        AssertJUnit.assertTrue(tittle.contains("百度"));
+    }
+
+    @Step("login step name")
+    void login(){
+        System.out.println("login step");
+    }
 
     @Test
-    public void test(){
+    void demo2(){
+
+        StepResult stepResult=new StepResult();
+        stepResult.setStatus(Status.FAILED);
+        stepResult.setName("new step name");
+        Allure.getLifecycle().startStep("new step", stepResult);
+        Allure.getLifecycle().stopStep();
 
         driver.get("http://baidu.com");
         String tittle=driver.getTitle();
         AssertJUnit.assertTrue(tittle.contains("百度"));
     }
 
-    @BeforeTest
-    public void beforeTest(){
+    @BeforeClass
+    static void beforeTest(){
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         driver=new ChromeDriver();
     }
 
-    @AfterTest
-    public void afterTest(){
+    @AfterClass
+    static void afterTest(){
         driver.quit();
     }
 }
